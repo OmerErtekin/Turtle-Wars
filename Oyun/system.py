dadc7602551063af28271c12914e372718c52ebe
Ömer Ertekin <57711950+OmerErtekin@users.noreply.github.com>
2020-01-03 14:27:38 +0300

    Add files via upload

---
+++ Oyun/system.py
@@ -0,0 +1,14 @@
+import platform
+
+def sound(sound):
+    if (platform.system() == "Linux"):
+        import os
+        merged = "aplay "+sound+"&"
+        return os.system(merged)
+    elif (platform.system() == "Darwin"):
+        import os
+        merged = "afplay "+sound+"&"
+        return os.system(merged)
+    elif (platform.system() ==  "Windows"):
+        import winsound
+        return winsound.PlaySound(sound,winsound.SND_ASYNC)
